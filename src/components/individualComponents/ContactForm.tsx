0bc611f467c964f2913c29ee53e980fd1ea741ac
Paul Jhon Magbanua <magbanuatx56@gmail.com>
2025-12-09 20:08:43 +0800

    change the form method from get to post

--- src/components/individualComponents/ContactForm.tsx
+++ src/components/individualComponents/ContactForm.tsx
@@ -73,6 +73,7 @@ export default function ContactForm() {
     <form
       onSubmit={handleSubmit}
       className="bg-black text-white rounded-2xl mt-15 px-6 py-6 w-[90%] max-w-md lg:mr-70 md:ml-10 sm:justify-self-center order-2 sm:order-1 max-sm:order-1 lg:order-2"
+      method="POST"
     >
       <h1 className="text-green text-header text-center font-bold font-primary">
         Contact Me!
