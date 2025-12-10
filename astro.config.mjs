b568082d07cc83a9fa4e7dc97c783010df0e30bb
Paul Jhon Magbanua <magbanuatx56@gmail.com>
2025-12-10 21:25:13 +0800

    test 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://magbanua-pauljhon.website' ,
   integrations: [react(),],
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
